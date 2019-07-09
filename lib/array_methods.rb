2b7adf001183496e443c6dd31263138900a59206
Andrew Santos <asantos@alumni.nd.edu>
2019-07-09 18:36:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   counter = 0 
   while array[counter]  do
-  #  if array[counter] == value_to_find
+    if array[counter] == value_to_find
+    return counter
     counter += 1
     end 
  # end
