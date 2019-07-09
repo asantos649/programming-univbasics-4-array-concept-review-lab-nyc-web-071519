2f860f8637798dce17f4fa26da8d785eab908265
Andrew Santos <asantos@alumni.nd.edu>
2019-07-09 18:33:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0 
+  while counter < array.length
+  #  if array[counter] == value_to_find
+      counter +=
+    end 
+ # end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.sort[array.length-1]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.sort[0]
 end
